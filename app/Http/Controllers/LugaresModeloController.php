50b1598d2b1be9f7852383be3a7e9b770947c813
SebastianAlvarez2025 <ja4971925@gmail.com>
2025-10-31 02:20:26 -0500

    Actualización tabla lugares

---
+++ app/Http/Controllers/LugaresModeloController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\lugaresModelo;
+use Illuminate\Http\Request;
+
+class LugaresModeloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(lugaresModelo $lugaresModelo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(lugaresModelo $lugaresModelo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, lugaresModelo $lugaresModelo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(lugaresModelo $lugaresModelo)
+    {
+        //
+    }
+}

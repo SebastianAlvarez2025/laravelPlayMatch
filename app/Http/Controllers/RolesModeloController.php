873a471845a66d689a9d17154ca18eb829800d08
kevinc2001 <kkevincerpaa@gmail.com>
2025-10-31 11:56:56 -0500

    Modulo Roles

---
+++ app/Http/Controllers/RolesModeloController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\rolesModelo;
+use Illuminate\Http\Request;
+
+class RolesModeloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(rolesModelo $rolesModelo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(rolesModelo $rolesModelo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, rolesModelo $rolesModelo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(rolesModelo $rolesModelo)
+    {
+        //
+    }
+}

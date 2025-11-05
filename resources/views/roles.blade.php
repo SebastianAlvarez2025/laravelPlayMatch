62c19156d68da018780176e38e1db4233956b05e
kevinc2001 <kkevincerpaa@gmail.com>
2025-11-05 13:55:13 -0500

    kevin

--- resources/views/roles.blade.php
+++ resources/views/roles.blade.php
@@ -1,75 +1,68 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title>Modulo Roles</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/fontawesome.min.css" integrity="sha512-M5Kq4YVQrjg5c2wsZSn27Dkfm/2ALfxmun0vUE3mPiJyK53hQBHYCVAtvMYEC7ZXmYLg8DVG4tF8gD27WmDbsg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Módulo Roles</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
-    <container class="container-sm d-flex justify-content-center mt-5">
-        <div class="card">
-            <div class="card-body" style="width: 1200px;">
-                <h3>Modulo Roles</h3>
+    <div class="container-sm d-flex justify-content-center mt-5">
+        <div class="card" style="width: 1200px;">
+            <div class="card-body">
+                <h3>Módulo Roles</h3>
                 <hr>
-                <form name="cliente" action="" method="post">
+                <form name="cliente" action="{{ url('/roles') }}" method="GET">
                     <div class="text-end mb-3">
                         <button type="button" class="btn btn-primary"><i class="fa-solid fa-plus"></i> Nuevo</button>
                     </div>
+
                     <div class="row g-2 align-items-center">
                         <div class="col-md-6">
                             <div class="input-group mb-3">
-                                <span class="input-group-text" id="basic-addon1">Buscar</span>
-                                <input type="text" class="form-control" placeholder="Buscar por usuario" aria-label="Username" aria-describedby="basic-addon1">
+                                <span class="input-group-text"><i class="fas fa-search"></i></span>
+                                <input type="text" class="form-control" name="search" value="{{ request('search') }}" placeholder="Buscar por Roles">
                             </div>
                         </div>
-
                         <div class="col-md-6 text-end">
-                            <button type="button" class="btn btn-info"><i class="fas fa-search-plus"></i> Buscar</button>
-                            <button type="button" class="btn btn-warning"><i class="fas fa-list"></i> Reset</button>
+                            <button type="submit" class="btn btn-info"><i class="fas fa-search-plus"></i> Buscar</button>
+                            <a href="{{ url('/roles') }}" class="btn btn-warning"><i class="fas fa-list"></i> Reset</a>
                         </div>
                     </div>
-
                 </form>
 
-                <table class="table table-striped table-hover table-bordered ">
+                @if($datos->count() > 0)
+                    <table class="table table-striped table-hover table-bordered">
                         <thead class="table-primary">
                             <tr>
-                            <th scope="col">Identificación del rol</th>
-                            <th scope="col">Nombre del rol</th>
-                            <th scope="col">Descripcion</th>
-                            <th>Acciones</th>
+                                <th>Identificación del rol</th>
+                                <th>Nombre del rol</th>
+                                <th>Descripción</th>
+                                <th>Acciones</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($datos as $item)
-                            <tr>
-                                <td>{{$item->id_rol}}</td>
-                                <td>{{$item->nombrerol}}</td>
-                                <td>{{$item->descripcion}}</td>
-                                <td>
-                                    <button type="button" class="btn btn-success"><i class="fa-solid fa-pen-to-square"></i> Editar</button>
-                                    <button type="button" class="btn btn-danger"><i class="fa-solid fa-trash"></i> Eliminar</button>
-                                </td>
-                           </tr>
+                                <tr>
+                                    <td>{{ $item->id_rol }}</td>
+                                    <td>{{ $item->nombrerol }}</td>
+                                    <td>{{ $item->descripcion }}</td>
+                                    <td>
+                                        <button type="button" class="btn btn-success"><i class="fa-solid fa-pen-to-square"></i> Editar</button>
+                                        <button type="button" class="btn btn-danger"><i class="fa-solid fa-trash"></i> Eliminar</button>
+                                    </td>
+                                </tr>
                             @endforeach
                         </tbody>
-                </table>
-                <nav aria-label="Page navigation example">
-                <ul class="pagination justify-content-end">
-                    <li class="page-item disabled">
-                    <a class="page-link">Previous</a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item">
-                    <a class="page-link" href="#">Next</a>
-                    </li>
-                </ul>
-                </nav>
+                    </table>
+                    <div class="d-flex justify-content-end">
+                        {{ $datos->links() }}
+                    </div>
+                @else
+                    <p class="text-center mt-3">No se encontraron roles.</p>
+                @endif
             </div>
         </div>
-    </container>
+    </div>
 </body>
 </html>

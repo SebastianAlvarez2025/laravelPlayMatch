114657afe3ec7ed4893432fecb71806ac0d67cea
georgembc321-afk <georgembc321@gmail.com>
2025-11-06 11:01:29 -0500

    arreglos

--- app/Http/Controllers/ResultadosModelController.php
+++ app/Http/Controllers/ResultadosModelController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace app\Http\Controllers;
 
-use App\Models\resultadoModel;
+use app\Models\resultadoModel;
 use Illuminate\Http\Request;
 
 class ResultadoModelController extends Controller

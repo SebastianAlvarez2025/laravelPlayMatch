69dbabd2768b0089bbd2cef8962d45e2ecfc7629
georgembc321-afk <georgembc321@gmail.com>
2025-11-12 11:46:59 -0500

    cambios vista categorias

--- app/Http/Controllers/categoriasController.php
+++ app/Http/Controllers/categoriasController.php
@@ -5,10 +5,41 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class categoriasController extends Controller
+class CategoriasController extends Controller
 {
-    public function index(){
-        $datos = DB::select("select * from categorias");
-        return view("categorias")->with("datos", $datos);
+    public function index()
+    {
+        $datos = DB::table('categorias')->get(); 
+        return view('categorias', compact('datos'));
+    }
+
+    public function store(Request $request)
+    {
+        DB::table('categorias')->insert([
+            'nombre_categoria' => $request->nombre_categoria,
+            'descripcion' => $request->descripcion,
+            'edad_minima' => $request->edad_minima,
+            'edad_maxima' => $request->edad_maxima,
+        ]);
+
+        return redirect()->route('categorias.index');
+    }
+
+    public function update(Request $request, $id)
+    {
+        DB::table('categorias')->where('id_categoria', $id)->update([
+            'nombre_categoria' => $request->nombre_categoria,
+            'descripcion' => $request->descripcion,
+            'edad_minima' => $request->edad_minima,
+            'edad_maxima' => $request->edad_maxima,
+        ]);
+
+        return redirect()->route('categorias.index');
+    }
+
+    public function destroy($id)
+    {
+        DB::table('categorias')->where('id_categoria', $id)->delete();
+        return redirect()->route('categorias.index');
     }
 }

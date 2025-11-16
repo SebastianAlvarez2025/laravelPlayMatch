cc3a80deaa4ad49dfe9e87f39c197cf096e04d36
georgembc321-afk <georgembc321@gmail.com>
2025-11-16 16:50:00 -0500

    cambios modulos2

--- app/Http/Controllers/equiposController.php
+++ app/Http/Controllers/equiposController.php
@@ -21,6 +21,20 @@ class equiposController extends Controller
             ->orderBy('id_equipo', 'asc')
             ->paginate(10);
 
+        $escudosMap = [
+            'Tigres FC' => 'https://dimayor.com.co/wp-content/uploads/2024/06/TIGRES-FC.png',
+            'junior FC' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/ESCUDO_JUNIOR.svg/1024px-ESCUDO_JUNIOR.svg.png',
+            'inter miami FC' =>'https://elparquedelosdibujos.com/colorear/dibujos-colorear-deportes/futbol/escudos-de-futbol/escudos-de-futbol-img/escudo-del-inter-miami-club-de-futbol-version-2-.webp',
+            'las trenzas del calvo' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8AzsB9oyEwivSquj0Y_YanemnexyhorutGuEuXCj-UpBHQ4C__OJvOTNhigGTMPNu4NQ&usqp=CAU',
+            'barcelona FC' => 'https://static.vecteezy.com/system/resources/thumbnails/014/414/712/small/fc-barcelona-logo-on-transparent-background-free-vector.jpg'
+        ];
+
+        // Asignar URL de escudos a cada equipo
+        $datos->getCollection()->transform(function ($equipo) use ($escudosMap) {
+            $equipo->escudo_final = $escudosMap[$equipo->nombre_equipo] ?? $equipo->escudo_url;
+            return $equipo;
+        });
+
         // PARA SELECT EN MODAL CREAR Y EDITAR
         $categorias = DB::table('categorias')->get();
 

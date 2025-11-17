1adadd02d2dd0768a1f52042e9d9fdda73336a63
Jesus-rcg <canonjesus931@gmail.com>
2025-11-17 16:03:50 -0500

    Vistas tipo_falta, tecnicos,

--- resources/views/tecnicos.blade.php
+++ resources/views/tecnicos.blade.php
@@ -1,80 +1,158 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modulo tecnicos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/fontawesome.min.css" integrity="sha512-M5Kq4YVQrjg5c2wsZSn27Dkfm/2ALfxmun0vUE3mPiJyK53hQBHYCVAtvMYEC7ZXmYLg8DVG4tF8gD27WmDbsg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-<body>
-    <container class="container-sm d-flex justify-content-center mt-5">
-        <div class="card">
-            <div class="card-body" style="width: 1200px;">
-                <h3>Modulo tecnicos</h3>
-                <hr>
-                <form name="cliente" action="" method="post">
-                    <div class="text-end mb-3">
-                        <button type="button" class="btn btn-primary"><i class="fa-solid fa-plus"></i> Nuevo</button>
-                    </div>
-                    <div class="row g-2 align-items-center">
-                        <div class="col-md-6">
-                            <div class="input-group mb-3">
-                                <span class="input-group-text" id="basic-addon1">Buscar</span>
-                                <input type="text" class="form-control" placeholder="Buscar por nombre o documento" aria-label="Username" aria-describedby="basic-addon1">
-                            </div>
-                        </div>
+@extends('welcome')
 
-                        <div class="col-md-6 text-end">
-                            <button type="button" class="btn btn-info"><i class="fas fa-search-plus"></i> Buscar</button>
-                            <button type="button" class="btn btn-warning"><i class="fas fa-list"></i> Reset</button>
+@section('title', 'tecnicos')
+@section('content')
+<div class="container-sm d-flex justify-content-center mt-5">
+    <div class="card" style="width: 1200px;">
+        <div class="card-body">
+            <h3>Módulo Tecnicos</h3>
+            <hr>
+
+            <form action="{{ url('/tecnicos') }}" method="GET">
+                <div class="text-end mb-3">
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#agregarModal">
+                        <i class="fa-solid fa-plus"></i> Nuevo
+                    </button>
+                </div>
+
+                <div class="row g-2 align-items-center">
+                    <div class="col-md-6">
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fas fa-search"></i></span>
+                            <input type="text" class="form-control" name="search" value="{{ request('search') }}" placeholder="Buscar por Id, Nombre o Equipo ">
                         </div>
                     </div>
+                    <div class="col-md-6 text-end">
+                        <button type="submit" class="btn btn-info"><i class="fas fa-search-plus"></i> Buscar</button>
+                        <a href="{{ url('/tecnicos') }}" class="btn btn-warning"><i class="fas fa-list"></i> Reset</a>
+                    </div>
+                </div>
+            </form>
 
-                </form>
-
-                <table class="table table-striped table-hover table-bordered ">
-                        <thead class="table-primary">
-                            <tr>
-                            <th scope="col">id_tecnico</th>
-                            <th scope="col">id_usuario</th>
-                            <th scope="col">id_equipo</th>
+            @if($datos->count() > 0)
+                <table class="table table-striped table-hover table-bordered">
+                    <thead class="table-primary">
+                    <tr>
+                       <th scope="col">Tecnico</th>
+                            <th scope="col">Usuario</th>
+                            <th scope="col">Equipo</th>
                             <th scope="col">licencia</th>
-                            <th scope="col">fecha_inicio</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($datos as $item)
-                            <tr>
-                                <td>{{$item->id_tecnico}}</td>
+                            <th scope="col">Fecha de inicio</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($datos as $item)
+                        <tr>
+                            <td>{{$item->id_tecnico}}</td>
                                 <td>{{$item->id_usuario}}</td>
                                 <td>{{$item->id_equipo}}</td>
                                 <td>{{$item->licencia}}</td>
-                                <td>{{$item->fecha_inicio}}</td>
-                                <td>
-                                    <button type="button" class="btn btn-success"><i class="fa-solid fa-pen-to-square"></i> Editar</button>
-                                    <button type="button" class="btn btn-danger"><i class="fa-solid fa-trash"></i> Eliminar</button>
-                                </td>
-                           </tr>
-                            @endforeach
-                        </tbody>
+                                <td>{{$item->fecha_inicio}}</td>                        
+                               <td>
+                                <!-- BOTÓN EDITAR -->
+                                <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#editarModal{{ $item->id_tecnico }}">
+                                    <i class="fa-solid fa-pen-to-square"></i> Editar
+                                </button>
+
+                                <!-- BOTÓN ELIMINAR -->
+                                <form action="{{ route('tecnicos.destroy', $item->id_tecnico) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger" onclick="return confirm('¿Seguro que deseas eliminar Tecnico ?')">
+                                        <i class="fa-solid fa-trash"></i> Eliminar
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+
+                        <!-- MODAL EDITAR -->
+                        <div class="modal fade" id="editarModal{{ $item->id_tecnico }}" tabindex="-1" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                                <div class="modal-content">
+                                    <form action="{{ route('tecnicos.update', $item->id_tecnico) }}" method="POST">
+                                        @csrf
+                                        @method('PUT')
+                                        <div class="modal-header">
+                                            <h5 class="modal-title"><i class="fa-solid fa-pen-to-square"></i> Editar Tecnico</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="mb-3">
+                                                <label for="id_usuario" class="form-label">Id Usuario</label>
+                                                <input type="text" class="form-control" name="id_usuario" value="{{ $item->id_usuario }}" required>
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="id_equipo " class="form-label">Id Equipo</label>
+                                                <input type="text" class="form-control" name="id_equipo " value="{{ $item->id_equipo  }}" required>
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="licencia" class="form-label">Licencia</label>
+                                                <input type="number" class="form-control" name="licencia" value="{{ $item->licencia }}" required>
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="fecha_inicio" class="form-label">Fecha de Inicio</label>
+                                                <input type="fecha_inicio" class="form-control" name="fecha_inicio" value="{{ $item->fecha_inicio }}" required>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                            <button type="submit" class="btn btn-primary"><i class="fa-solid fa-floppy-disk"></i> Guardar cambios</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                    </tbody>
                 </table>
-                <nav aria-label="Page navigation example">
-                <ul class="pagination justify-content-end">
-                    <li class="page-item disabled">
-                    <a class="page-link">Previous</a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item">
-                    <a class="page-link" href="#">Next</a>
-                    </li>
-                </ul>
-                </nav>
+            @else
+                <p class="text-center mt-3">No se encontro Tecnico.</p>
+            @endif
+        </div>
+
+        <!-- MODAL AGREGAR -->
+        <div class="modal fade" id="agregarModal" tabindex="-1" aria-labelledby="agregarModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <form action="{{ route('tecnicos.store') }}" method="POST">
+                        @csrf
+                        <div class="modal-header">
+                            <h5 class="modal-title"><i class="fa-solid fa-user"></i> Crear Tecnico</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="mb-3">
+                                <label for="id_tecnico" class="form-label">Id Tecnico</label>
+                                <input type="text" class="form-control" name="id_tecnico" placeholder="Ingrese el ID del tecnico" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="id_usuario" class="form-label">Id Usuario</label>
+                                <input type="text" class="form-control" name="id_usuario" placeholder="Ingrese el ID de usuario" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="id_equipo" class="form-label">Id Equipo</label>
+                                <input type="text" class="form-control" name="id_equipo" placeholder="Ingrese el ID de equipo" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="licencia" class="form-label">Licencia</label>
+                                <input type="number" class="form-control" name="licencia" placeholder="Ingrese licencia" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="fecha_inicio" class="form-label">Fecha de Inicio</label>
+                                <input type="number" class="form-control" name="fecha_inicio" placeholder="Ingrese fecha de inicio" required>
+                            </div>
+                            
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                            <button type="submit" class="btn btn-primary"><i class="fa-solid fa-floppy-disk"></i> Guardar</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </container>
-</body>
-</html>
+
+    </div>
+</div>
+
+@endsection
